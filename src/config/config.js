36af1f92f04802f337bb7754576b8be03ae4bd92
dancar528 <dancar.528@gmail.com>
2019-07-07 12:49:41 -0500

    Modifying connection strings to mongo db

--- src/config/config.js
+++ src/config/config.js
@@ -1,11 +1,12 @@
 process.env.PORT = process.env.PORT || 3000;
 process.env.NODE_ENV = process.env.NODE_ENV || 'local';
+process.env.URLDB = process.env.URLDB || 'mongodb://localhost:27017/educacionContinua';
 
-let urlDB;
+/*let urlDB;
 if (process.env.NODE_ENV === 'local'){
 	urlDB = 'mongodb://localhost:27017/educacionContinua';
 } else {
-	urlDB = 'mongodb+srv://dcardoh:Mayo2018@nodejstdea-f8r7w.mongodb.net/educacionContinua?retryWrites=true&w=majority';
+	urlDB = '';
 }
 
-process.env.URLDB = urlDB;
+process.env.URLDB = urlDB;*/
