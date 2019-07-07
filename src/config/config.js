5e25c86f628ff915091c558725b0b13d11c7cac8
dancar528 <dancar.528@gmail.com>
2019-07-07 11:10:05 -0500

    Setting connection string to connect to mongodb in atlas from heroku

--- src/config/config.js
+++ src/config/config.js
@@ -1,1 +1,11 @@
-process.env.URLDB = 'mongodb://localhost:27017/educacionContinua'
+process.env.NODE_ENV = process.env.NODE_ENV || 'local';
+
+let urlDB;
+if (process.env.NODE_ENV === 'local'){
+	urlDB = 'mongodb://localhost:27017/educacionContinua';
+}
+else {
+	urlDB = 'mongodb+srv://dcardoh:Mayo2018@nodejstdea-f8r7w.mongodb.net/educacionContinua?retryWrites=true&w=majority';
+}
+
+process.env.URLDB = urlDB;
